6874b309b963d579b4fefd2c3de2129f97f73083
Domantas Bieliūnas <domantas231@gmail.com>
2025-11-26 23:36:51 +0200

    fix: toast styling

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import App from './App.tsx'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
+import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css' 
+import 'primereact/resources/primereact.min.css' 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
